9a1821ac50092b4ce1b5942aa2894c1d6b4bbc9c
kongzhy <kongzhaoyang77@icloud.com>
2016-03-20 19:58:47 +0800

    更新进度

---
+++ Book/Effective Objective-C 2.0/06-理解"属性"这一概念/propertyTest/propertyTest/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  propertyTest
+//
+//  Created by Conway on 16/3/19.
+//  Copyright © 2016年 JDJR. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
